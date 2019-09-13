924729772b76dd1e4b796279d7ae9ded828fb439
CScori <amilanotte1366@gmail.com>
2019-09-13 11:44:09 -0500

    adds map of single api call

--- starwars/src/components/WarsList.js
+++ starwars/src/components/WarsList.js
@@ -5,25 +5,25 @@ import WarsCard from './WarsCard';
 const WarsList = () => {
     const [char, setChar] = useState([]);
 
-    // useEffect(() => {
-    //     axios
-    //     .get('https://swapi.co/api/people/')
-    //     .then(res => {
-    //       console.log(res.data.results)
-    //       const chars = res.data.response
-    //       setChar(chars)
-    //       console.log('set character return', chars)
-    //     })
-    //     .catch(error => {
-    //       console.error('Server Error', error);
-    //     })
-    // }, []);
+    useEffect(() => {
+        axios
+        .get('https://swapi.co/api/people/')
+        .then(res => {
+          const chars = res.data.results
+          setChar(chars)
+          console.log('set character return', chars)
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        })
+    }, []);
 
     return (
         <div>
-            <div>
             {char.map(data => {
-                return <WarsCard key={data.id}
+                return (
+                <WarsCard 
+                key={data.id}
                 name={data.name}
                 height={data.height}
                 hair={data.hair_color}
@@ -31,10 +31,9 @@ const WarsList = () => {
                 sex={data.gender}
                 born={data.bith_year}
                 
-                
                 />
+                )
             })} 
-            </div>
         </div>
     )
 }
