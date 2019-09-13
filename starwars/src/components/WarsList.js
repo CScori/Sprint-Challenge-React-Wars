e682ff9df6f7ca586d134acd55f5ba497e88cd25
CScori <amilanotte1366@gmail.com>
2019-09-13 11:17:20 -0500

    adds state and effect

--- starwars/src/components/WarsList.js
+++ starwars/src/components/WarsList.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import WarsCard from './WarsCard';
 
 const WarsList = () => {
+    const [char, setChar] = useState([]);
+
+    // useEffect(() => {
+    //     axios
+    //     .get('https://swapi.co/api/people/')
+    //     .then(res => {
+    //       console.log(res.data.results)
+    //       const chars = res.data.response
+    //       setChar(chars)
+    //       console.log('set character return', chars)
+    //     })
+    //     .catch(error => {
+    //       console.error('Server Error', error);
+    //     })
+    // }, []);
+
     return (
         <div>
             
